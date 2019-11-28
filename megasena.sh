d5ffd0f7d50dd6864b3859621dab73c700434710
mcyberlinux <mcyberlinux@mariodba.opensystems>
2019-11-27 23:55:42 -0300

    megasena.sh

--- megasena.sh
+++ megasena.sh
@@ -3,77 +3,77 @@
 ######################################
 # Gera dezenas de 1 até 10:
 echo "Dezena1"
-random_now=$(for ((n="1";$n<10;n++));
+sorte_grande=$(for ((n="1";$n<10;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ######################################
 # Gera dezenas de 11 até 20:
 echo "Dezena2"
-random_now=$(for ((n="11";$n<20;n++));
+sorte_grande=$(for ((n="11";$n<20;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ######################################
 # Gera dezenas de 21 até 30:
 echo "Dezena3"
-random_now=$(for ((n="21";$n<30;n++));
+sorte_grande=$(for ((n="21";$n<30;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ######################################
 # Gera dezenas de 31 até 40:
 echo "Dezena4"
-random_now=$(for ((n="31";$n<40;n++));
+sorte_grande=$(for ((n="31";$n<40;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ######################################
 # Gera dezenas de 41 até 50:
 echo "Dezena5"
-random_now=$(for ((n="41";$n<50;n++));
+sorte_grande=$(for ((n="41";$n<50;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ######################################
 # Gera dezenas de 51 até 60:
 echo "Dezena6"
-random_now=$(for ((n="51";$n<60;n++));
+sorte_grande=$(for ((n="51";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ##########################################
 # Gera dezenas de 1 até 60 aleatóriamente:
 echo "Números da Sorte!"
-random_now=$(for ((n="1";$n<60;n++));
+sorte_grande=$(for ((n="1";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ###########################################
-random_now=$(for ((n="1";$n<60;n++));
+sorte_grande=$(for ((n="1";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ###########################################
-random_now=$(for ((n="1";$n<60;n++));
+sorte_grande=$(for ((n="1";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ###########################################
-random_now=$(for ((n="1";$n<60;n++));
+sorte_grande=$(for ((n="1";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ###########################################
-random_now=$(for ((n="1";$n<60;n++));
+sorte_grande=$(for ((n="1";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ###########################################
-random_now=$(for ((n="1";$n<60;n++));
+sorte_grande=$(for ((n="1";$n<60;n++));
 do echo "$n";
 done | sort -R | head -n1)
-echo "$random_now"
+echo "$sorte_grande"
 ###########################################
 echo "Boa sorte!"
 ###########################################
